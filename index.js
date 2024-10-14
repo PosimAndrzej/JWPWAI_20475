b7fdeef054500018cf3b0f6cbdd985907cb000b2
PosimAndrzej <posimandrzej@gmail.com>
2024-10-14 11:23:55 +0200

    lab3 zad4

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-const http = require('http');
-const path = require("path");
-const util = require("util");
-const v8 = require("v8");
-const hostname = '127.0.0.1';
-const port = 8888;
-const server = http.createServer((request, response) => {
- response.statusCode = 200;
- response.setHeader('Content-Type', 'text/plain');
- response.end('Hello, from node.js serwer !\n');
+import stripAnsi from 'strip-ansi';
 
-});
-server.listen(port, hostname, () => {
- util.log(v8.getHeapStatistics());
- console.log(path.basename(__filename));
- util.log(path.join(__dirname, 'uploads','images'));
- console.log(`Server running at http://${hostname}:${port}/`);
-});
+console.log('stripAnsi, jest modułem, który obcina ANSI escape code z tekstu. \nPozwala to na pozbycie się formatowania tekstu.');
+
+console.log('\u001B[4mUnicorn\u001B[0m');
+//=> 'Unicorn'
+
+console.log(stripAnsi('\u001B[4mUnicorn\u001B[0m'));
+//=> 'Unicorn'
+
+console.log('\u001B]8;;https://github.com\u0007Click\u001B]8;;\u0007');
+//=> 'Click'
+
+console.log(stripAnsi('\u001B]8;;https://github.com\u0007Click\u001B]8;;\u0007'));
+//=> 'Click'
+
