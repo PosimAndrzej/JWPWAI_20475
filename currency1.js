6aeb66730d85015bc848f311daa858ff302f3609
PosimAndrzej <posimandrzej@gmail.com>
2024-10-28 10:23:51 +0100

    Lab 4 zad 1 wersja 1
    podejście przez referencje do obiektu export

---
+++ currency1.js
@@ -0,0 +1,24 @@
+let canadianDollar = 2.89;
+let usDollar = 4.02;
+let gbp = 5.21;
+let eur = 4.35;
+let czk = 0.17;
+
+function roundTwoDecimals(amount) {
+    return Math.round(amount * 100) / 100;
+}
+exports.canadianToPLN = function(canadian) {
+    return roundTwoDecimals(canadian * canadianDollar);
+}
+exports.USToPLN = function(us) {
+    return roundTwoDecimals(us * usDollar);
+}
+exports.GBPToPLN = function(funt) {
+    return roundTwoDecimals(funt * gbp);
+}
+exports.EURToPLN = function(euro) {
+    return roundTwoDecimals(euro * eur);
+}
+exports.CZKToPLN = function(korona) {
+    return roundTwoDecimals(korona * czk);
+}
---
+++ module_test.js
@@ -0,0 +1,16 @@
+let currency = require('./currency1');
+
+console.log('50 dolarów kanadyjskich odpowiada następującej ilości złotych:');
+console.log(currency.canadianToPLN(50));
+
+console.log('50 dolarów amerykańskich odpowiada następującej ilości złotych:');
+console.log(currency.USToPLN(50));
+
+console.log('50 funtów brytyjskich odpowiada następującej ilości złotych:');
+console.log(currency.GBPToPLN(50));
+
+console.log('50 euro odpowiada następującej ilości złotych:');
+console.log(currency.EURToPLN(50));
+
+console.log('50 koron czeskich odpowiada następującej ilości złotych:');
+console.log(currency.CZKToPLN(50));
